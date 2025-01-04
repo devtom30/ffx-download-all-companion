362529d2bd4d9844607a1420c64348cf3689860b
devtom30 <devtom30@gmail.com>
2025-01-04 17:31:26 +0100

    not working with chromium native messaging

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,58 @@
 use std::io;
 use std::sync::mpsc;
-use std::sync::mpsc::Receiver;
+use std::sync::mpsc::{Receiver, SendError};
 use std::sync::mpsc::TryRecvError;
 use std::{thread, time};
+use std::io::Write;
+use log4rs::init_file;
+use log::warn;
 
 fn main() {
+    match init_file("/home/tom/RustroverProjects/ffx-download-all-companion/log4rs.yml", Default::default()) {
+        Ok(_) => {}
+        Err(e) => { println!("{e}")}
+    }
+
+    warn!("launching thread…");
+
     let stdin_channel = spawn_stdin_channel();
     loop {
         match stdin_channel.try_recv() {
-            Ok(key) => println!("Received: {}", key),
-            Err(TryRecvError::Empty) => println!("Channel empty"),
+            Ok(key) => {
+                // println!("Received: {}", key);
+                warn!("Received: {}", key);
+                println!("{key}")
+            },
+            Err(TryRecvError::Empty) => {
+                // println!("Channel empty");
+                warn!("Channel empty")
+            },
             Err(TryRecvError::Disconnected) => panic!("Channel disconnected"),
         }
-        sleep(1000);
+        // io::stdout().write_all(b"hello world").expect("TODO: panic message");
+        sleep(2000);
     }
+
+    warn!("quit");
 }
 
 fn spawn_stdin_channel() -> Receiver<String> {
     let (tx, rx) = mpsc::channel::<String>();
     thread::spawn(move || loop {
         let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).unwrap();
-        tx.send(buffer).unwrap();
+        match io::stdin().read_line(&mut buffer) {
+            Ok(n) => {
+                if n > 0 {
+                    warn!("{n} bytes read");
+                    warn!("{buffer}");
+                }
+            }
+            Err(error) => warn!("error: {error}"),
+        }
+        match tx.send(String::from("reçu le message, gros")) {
+            Ok(_) => {}
+            Err(e) => {warn!("{e}");}
+        }
     });
     rx
 }
