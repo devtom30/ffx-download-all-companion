3a1bc0f38cd69898be2abb4ef7ec01e2d4a6116c
devtom30 <devtom30@gmail.com>
2025-01-04 18:05:38 +0100

    working but just blocking on stdin on the main thread

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
-use std::io;
+use std::{thread, time};
+use std::io::{self, BufRead};
 use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
-use std::sync::mpsc::TryRecvError;
-use std::{thread, time};
+
+use log4rs::init_file;
+use log::warn;
+
+mod lib;
 
 fn main() {
-    let stdin_channel = spawn_stdin_channel();
+    match init_file("/home/tom/RustroverProjects/ffx-download-all-companion/log4rs.yml", Default::default()) {
+        Ok(_) => {}
+        Err(e) => { println!("{e}")}
+    }
+    warn!("launched");
+
     loop {
-        match stdin_channel.try_recv() {
-            Ok(key) => println!("Received: {}", key),
-            Err(TryRecvError::Empty) => println!("Channel empty"),
-            Err(TryRecvError::Disconnected) => panic!("Channel disconnected"),
-        }
-        sleep(1000);
+        let json_val = match lib::read_input(io::stdin()) {
+            Err(why) => panic!("{}", why.to_string()),
+            Ok(json_val) => json_val,
+        };
+        //if json_val == "ping" {
+        // your code here
+        let response = serde_json::json!({ "text": "pong" });
+        match lib::write_output(io::stdout(), &response) {
+            Err(why) => panic!("{}", why.to_string()),
+            Ok(_) => (),
+        };
+        //}
     }
 }
+
 
 fn spawn_stdin_channel() -> Receiver<String> {
     let (tx, rx) = mpsc::channel::<String>();
