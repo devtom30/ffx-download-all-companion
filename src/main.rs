743120e9c12b05a1b62c40d90dea7c297dd115c3
devtom30 <devtom30@gmail.com>
2025-04-20 11:40:37 +0200

    reading stdin in thread, doing things in other thread and in main thread: looping for messages sent through channels by threads and writing in stdout (to send messages to chrome extension via native messaging)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::{thread, time};
-use std::collections::VecDeque;
-use std::io::{self, BufRead};
-use std::sync::{Arc, LockResult, mpsc, Mutex};
-use std::sync::mpsc::{Receiver, Sender, TryRecvError};
-
+use log::warn;
 use log4rs::append::Append;
 use log4rs::init_file;
-use log::warn;
+use std::collections::VecDeque;
+use std::io::{self, BufRead};
+use std::sync::mpsc::{Receiver, Sender, TryRecvError};
+use std::sync::{mpsc, Arc, LockResult, Mutex};
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
+use std::{thread, time};
 
 mod lib;
 
@@ -22,8 +22,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
     let my_todo = todo.clone();
+    let tx_input_reader = tx.clone();
     let worker = thread::spawn(move || {
-        loop {
+        /*loop {
             //let lock_result = todo.lock();
             if my_todo.lock().expect("uh").len() > 0 {
                 while let Some(request) = my_todo.lock().expect("uh").pop_front() {
@@ -35,29 +36,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 warn!("nothing to do now");
             }
             sleep(2000);
+        }*/
+
+        loop {
+            let json_val = match lib::read_input(io::stdin()) {
+                Err(why) => panic!("{}", why.to_string()),
+                Ok(json_val) => json_val,
+            };
+            //if json_val == "ping" {
+            // your code here
+
+            if let Some(text) = json_val.get("text") {
+                todo.lock().expect("uh").push_back(text.to_string());
+            }
+            let response = serde_json::json!({ "text": "pong" });
+            tx_input_reader.send(response.to_string()).unwrap();
+            //}
+        }
+    });
+
+    let worker2 = thread::spawn(move || {
+        loop {
+            let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+            let response = serde_json::json!({ "text": String::from("pong ") + now.to_string().as_str() });
+            tx.send(response.to_string()).unwrap();
+            sleep(2000);
         }
     });
 
     loop {
-        let json_val = match lib::read_input(io::stdin()) {
-            Err(why) => panic!("{}", why.to_string()),
-            Ok(json_val) => json_val,
-        };
-        //if json_val == "ping" {
-        // your code here
-
-        if let Some(text) = json_val.get("text") {
-            todo.lock().expect("uh").push_back(text.to_string());
-        }
-        let response = serde_json::json!({ "text": "pong" });
-        match lib::write_output(io::stdout(), &response) {
-            Err(why) => panic!("{}", why.to_string()),
-            Ok(_) => (),
-        };
-        //}
-
         match rx.try_recv() {
-            Ok(key) => warn!("Received from worker: {}", key),
+            Ok(response) => {
+                warn!("Received from worker: {}", response);
+                let response = serde_json::from_str(response.as_str()).unwrap();
+                match lib::write_output(io::stdout(), &response) {
+                    Err(why) => panic!("{}", why.to_string()),
+                    Ok(_) => (),
+                };
+            },
             Err(TryRecvError::Empty) => warn!("Channel empty"),
             Err(TryRecvError::Disconnected) => warn!("Channel disconnected"),
         }
